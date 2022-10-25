0ed6a39e774c3a2469dade353b5b54902ba7e196
Minh Trung <nmtrun1001@gmail.com>
2022-10-25 09:12:25 +0700

    Xay dung trang hieu chinh san pham

--- lib/ui/products/edit_product_screen.dart
+++ lib/ui/products/edit_product_screen.dart
@@ -99,24 +99,6 @@ class _EditProductScreenState extends State<EditProductScreen> {
     }
   }
 
-  Future<void> showErrorDialog(BuildContext context, String message) async {
-    return showDialog(
-      context: context,
-      builder: (ctx) => AlertDialog(
-        title: const Text('An Error Occurred!'),
-        content: Text(message),
-        actions: <Widget>[
-          TextButton(
-            child: const Text('Okay'),
-            onPressed: () {
-              Navigator.of(ctx).pop();
-            },
-          )
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/ui/shared/app_drawer.dart
+++ lib/ui/shared/app_drawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../auth/auth_manager.dart';
 import '../orders/order_screen.dart';
 import '../products/user_products_screen.dart';
 
@@ -41,6 +43,16 @@ class AppDrawer extends StatelessWidget {
                   .pushReplacementNamed(UserProductsScreen.routeName);
             },
           ),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Logout'),
+            onTap: () {
+              Navigator.of(context)
+                ..pop()
+                ..pushReplacementNamed('/');
+              context.read<AuthManager>().logout();
+            },
+          )
         ],
       ),
     );
