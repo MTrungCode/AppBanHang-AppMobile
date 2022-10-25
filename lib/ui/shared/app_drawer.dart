951a0311e7409056b3d4802e9019263caabe4451
Minh Trung <nmtrun1001@gmail.com>
2022-10-25 09:17:58 +0700

    Xay dung drawer va dinh tuyen theo ten

--- lib/ui/shared/app_drawer.dart
+++ lib/ui/shared/app_drawer.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../auth/auth_manager.dart';
 import '../orders/order_screen.dart';
 import '../products/user_products_screen.dart';
 
@@ -43,16 +41,6 @@ class AppDrawer extends StatelessWidget {
                   .pushReplacementNamed(UserProductsScreen.routeName);
             },
           ),
-          ListTile(
-            leading: const Icon(Icons.exit_to_app),
-            title: const Text('Logout'),
-            onTap: () {
-              Navigator.of(context)
-                ..pop()
-                ..pushReplacementNamed('/');
-              context.read<AuthManager>().logout();
-            },
-          )
         ],
       ),
     );
